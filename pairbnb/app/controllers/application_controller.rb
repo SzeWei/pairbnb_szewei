54f7528e0489cab2ea8163ef14a388613c92c079
Sze Wei Khoo <sze_wei92@hotmail.com>
2016-11-23 10:23:22 +0800

    instaled figaro gem

--- pairbnb/app/controllers/application_controller.rb
+++ pairbnb/app/controllers/application_controller.rb
@@ -2,5 +2,22 @@ class ApplicationController < ActionController::Base
   include Clearance::Controller
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  # before_action :require_login
+
+  def index
+    current_user.articles
+  end
+
+	# def url_after_denied_access_when_signed_in
+ #    Clearance.configuration.redirect_url
+ #  end
+
+ #  def url_after_denied_access_when_signed_out
+ #    sign_in_url
+ #  end
+  
   protect_from_forgery with: :exception
+
+  
 end
+
