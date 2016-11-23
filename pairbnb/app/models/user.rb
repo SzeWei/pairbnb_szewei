0f24b15ecf81328484b83f42ecb9dc60246f5236
Sze Wei Khoo <sze_wei92@hotmail.com>
2016-11-23 14:34:07 +0800

    signup fail

--- pairbnb/app/models/user.rb
+++ pairbnb/app/models/user.rb
@@ -5,8 +5,8 @@ class User < ActiveRecord::Base
 
   def self.create_with_auth_and_hash(authentication,auth_hash)
     create! do |u|
+      # u.first_name = auth_hash["info"]["first_name"]
       u.email = auth_hash["extra"]["raw_info"]["email"]
-      u.created_at = auth_hash["info"]["created_at"]
       u.authentications<<(authentication)
     end
   end
