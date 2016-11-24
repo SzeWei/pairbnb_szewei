826bc8347934e29c088182bf4ca2d96bd7d375b7
Sze Wei Khoo <sze_wei92@hotmail.com>
2016-11-24 23:11:34 +0800

    dummy data added to users and listings

--- pairbnb/db/seeds.rb
+++ pairbnb/db/seeds.rb
@@ -6,3 +6,23 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+10.times do 
+	user = User.new
+	user.first_name = Faker::Name.first_name
+	user.last_name = Faker::Name.last_name
+	user.email = Faker::Internet.email
+	user.password = "1234"
+		if user.save
+			number_of_listing = rand(0..3)
+			number_of_listing.times do
+				listing = Listing.new
+				listing.name = Faker::Company.name
+				listing.description = Faker::Company.catch_phrase
+				listing.address = Faker::Address.country
+				listing.price = Faker::Number.between(30, 1000)
+				user.listings << listing
+				user.save
+			end
+		end
+
+end 
