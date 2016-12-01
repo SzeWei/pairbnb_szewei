07c776860b8cb829033fee98deb77d7364bbbefa
SzeWei <sze_wei92@hotmail.com>
2016-12-01 16:46:31 +0800

    production.rb updated

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,17 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: "https://www.szewei-pairbnb.herokuapp.com" }
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.active_job.queue_name_prefix = "pairbnb"
+  config.active_job.queue_name_delimiter = "_"
+  config.action_mailer.smtp_settings = {
+  address:              'smtp.gmail.com',
+  port:                 587,
+  domain:               'localhost:3000',
+  user_name:            ENV['GMAIL_USERNAME'], 
+  password:             ENV['GMAIL_PASSWORD'],
+  authentication:       'plain',
+  enable_starttls_auto: true  }
+
 end
